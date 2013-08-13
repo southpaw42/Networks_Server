a0d9de19d84610741734da323410f6cfc2176abf
whit8024 <whit8024@kettering.edu>
2013-08-13 00:27:47 -0400

    Implemented echo server on port 27015

--- Networks_Server/Networks_Server.cpp
+++ Networks_Server/Networks_Server.cpp
@@ -3,11 +3,22 @@
 
 #include "stdafx.h"
 #include "Networks_Server.h"
+#include <winsock2.h>
+#include <WS2tcpip.h>
+#include <stdio.h>
+
+#pragma comment(lib, "Ws2_32.lib")
 
 #ifdef _DEBUG
 #define new DEBUG_NEW
 #endif
 
+#define DEFAULT_BUFLEN 512
+#define DEFAULT_PORT 27015
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
 
 // The one and only application object
 
@@ -15,6 +26,7 @@ CWinApp theApp;
 
 using namespace std;
 
+// The _tmain function is the actual entry point, but we shouldn't have code here
 int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 {
 	int nRetCode = 0;
@@ -43,4 +55,78 @@ int _tmain(int argc, TCHAR* argv[], TCHAR* envp[])
 	}
 
 	return nRetCode;
+}
+
+// This is our normal entry point
+int main()
+{
+    SOCKET s;
+    struct sockaddr_in server, si_other;
+    int slen , recv_len;
+    char buf[DEFAULT_BUFLEN];
+    WSADATA wsa;
+ 
+    slen = sizeof(si_other) ;
+     
+    //Initialise winsock
+    printf("\nInitialising Winsock...");
+    if (WSAStartup(MAKEWORD(2,2),&wsa) != 0)
+    {
+        printf("Failed. Error Code : %d",WSAGetLastError());
+        exit(EXIT_FAILURE);
+    }
+    printf("Initialised.\n");
+     
+    //Create a socket
+    if((s = socket(AF_INET , SOCK_DGRAM , 0 )) == INVALID_SOCKET)
+    {
+        printf("Could not create socket : %d" , WSAGetLastError());
+    }
+    printf("Socket created.\n");
+     
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( DEFAULT_PORT );
+     
+    //Bind
+    if( bind(s ,(struct sockaddr *)&server , sizeof(server)) == SOCKET_ERROR)
+    {
+        printf("Bind failed with error code : %d" , WSAGetLastError());
+        exit(EXIT_FAILURE);
+    }
+    puts("Bind done");
+ 
+    //keep listening for data
+    while(1)
+    {
+        printf("Waiting for data...");
+        fflush(stdout);
+         
+        //clear the buffer by filling null, it might have previously received data
+        memset(buf,'\0', DEFAULT_BUFLEN);
+         
+        //try to receive some data, this is a blocking call
+        if ((recv_len = recvfrom(s, buf, DEFAULT_BUFLEN, 0, (struct sockaddr *) &si_other, &slen)) == SOCKET_ERROR)
+        {
+            printf("recvfrom() failed with error code : %d" , WSAGetLastError());
+            exit(EXIT_FAILURE);
+        }
+         
+        //print details of the client/peer and the data received
+        printf("Received packet from %s:%d\n", inet_ntoa(si_other.sin_addr), ntohs(si_other.sin_port));
+        printf("Data: %s\n" , buf);
+         
+        //now reply the client with the same data
+        if (sendto(s, buf, recv_len, 0, (struct sockaddr*) &si_other, slen) == SOCKET_ERROR)
+        {
+            printf("sendto() failed with error code : %d" , WSAGetLastError());
+            exit(EXIT_FAILURE);
+        }
+    }
+ 
+    closesocket(s);
+    WSACleanup();
+     
+    return 0;
 }
